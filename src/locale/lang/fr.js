838cf182fedabb05c48caeb645e54a49d4f5eb87
hugohil <hugo@soixantecircuits.fr>
2016-11-16 15:43:55 +0100

    Create fr.js

---
+++ src/locale/lang/fr.js
@@ -0,0 +1,85 @@
+export default {
+  el: {
+    datepicker: {
+      now: 'Maintenant',
+      today: 'Aujourd\'hui',
+      cancel: 'Annuler',
+      clear: 'Effacer',
+      confirm: 'OK',
+      selectDate: 'Selectionnez une date',
+      selectTime: 'Selectionnez un horaire',
+      startDate: 'Date de début',
+      startTime: 'Horaire de début',
+      endDate: 'Date de fin',
+      endTime: 'Horaire de fin',
+      year: 'Année',
+      month1: 'Janvier',
+      month2: 'Février',
+      month3: 'Mars',
+      month4: 'Avril',
+      month5: 'Mai',
+      month6: 'Juin',
+      month7: 'Juillet',
+      month8: 'Août',
+      month9: 'Septembre',
+      month10: 'Octobre',
+      month11: 'Novembre',
+      month12: 'Decembre',
+      // week: 'semaine',
+      weeks: {
+        sun: 'Dim',
+        mon: 'Lun',
+        tue: 'Mar',
+        wed: 'Mer',
+        thu: 'Jeu',
+        fri: 'Ven',
+        sat: 'Sam'
+      },
+      months: {
+        jan: 'Jan',
+        feb: 'Fev',
+        mar: 'Mar',
+        apr: 'Avr',
+        may: 'Mai',
+        jun: 'Jun',
+        jul: 'Jul',
+        aug: 'Aug',
+        sep: 'Sep',
+        oct: 'Oct',
+        nov: 'Nov',
+        dec: 'Dec'
+      }
+    },
+    select: {
+      loading: 'Chargement',
+      noMatch: 'Pas de correspondance',
+      noData: 'Donnée introuvable',
+      placeholder: 'Selectionnez'
+    },
+    pagination: {
+      goto: 'Aller à',
+      pagesize: '/page',
+      total: 'Total {total}',
+      pageClassifier: ''
+    },
+    messagebox: {
+      confirm: 'OK',
+      cancel: 'Annuler',
+      error: 'Non autorisé'
+    },
+    upload: {
+      delete: 'Supprimer',
+      preview: 'Previsualiser',
+      continue: 'Continuer'
+    },
+    table: {
+      emptyText: 'Donnée introuvable',
+      confirmFilter: 'Confirmer',
+      resetFilter: 'Remettre à zéro',
+      clearFilter: 'Tous'
+    },
+    tree: {
+      emptyText: 'Donnée introuvable'
+    }
+  }
+};
